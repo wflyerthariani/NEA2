7f52aeda30f59bb225365ca24d6c238a985bd919
Arman Thariani <wflyerthariani@gmail.com>
2020-12-13 16:51:42 +0400

    Enabled searching by author

--- librarysite/libraryaccess/tables.py
+++ librarysite/libraryaccess/tables.py
@@ -2,14 +2,14 @@ import django_tables2 as tables
 from libraryaccess.models import Student, Author, Genre, Book
 
 class BookTable(tables.Table):
-    all_authors = tables.Column(verbose_name='Authors')
+    all_authors = tables.Column(verbose_name='Authors', order_by=("bookAuthor.surname"))
     go_to_view = tables.TemplateColumn('<a href="/libraryaccess/bookview/{{record.ISBN}}">View</a>')
     class Meta:
         model = Book
         fields = ['ISBN', 'title', 'all_authors']
 
 class MyBookTable(tables.Table):
-    all_authors = tables.Column(verbose_name='Authors')
+    all_authors = tables.Column(verbose_name='Authors', order_by=("bookAuthor.surname"))
     go_to_view = tables.TemplateColumn('<a href="/libraryaccess/bookview/{{record.ISBN}}">View</a>')
     delete_book = tables.TemplateColumn('<a href="/libraryaccess/removebook/{{record.ISBN}}">Remove</a>')
     class Meta:
