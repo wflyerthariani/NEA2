537f20c13b27b6498706221fddbeee46a7a76707
Arman Thariani <wflyerthariani@gmail.com>
2021-01-30 18:08:38 +0400

    Finalised for writeup

--- librarysite/libraryaccess/cron.py
+++ librarysite/libraryaccess/cron.py
@@ -1,6 +1,8 @@
 import csv
 from libraryaccess.models import Student, Author, Genre, Book
 
+export PATH='/Users/arman/Dev/NEAvenv/bin:/Users/arman/.cabal/bin:/Users/arman/.ghcup/bin:/Users/arman/opt/anaconda3/bin:/Users/arman/opt/anaconda3/condabin:/Library/Frameworks/Python.framework/Versions/3.7/bin:/Library/Frameworks/Python.framework/Versions/3.6/bin:/Library/Frameworks/Python.framework/Versions/3.7/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin'
+
 def my_cron_job():
     with open('libraryaccess/recommendation_info.csv', 'w') as recommendation_info:
         all_books = Book.objects.all().filter(inLibrary = True)
